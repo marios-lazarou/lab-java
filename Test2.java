a845507cfbedb9ef43f820cb8d600ce911bc90c0
Marios Lazarou <mariosl@pumapay.io>
2019-02-27 00:43:13 +0200

    exercise one completed

--- Test2.java
+++ Test2.java
@@ -8,6 +8,9 @@ public class Test2 {
 	/*
 	 * What will happen if these two methods are called by different threads?
 	 * 
+	 * The cases where the validation will fail are:
+	 * 1) In case that values in between dots are 3 digits and starting with 0 eg: 000
+	 * 2) Values that are more than 255
 	 */
 	public void mym1() {
 		synchronized (String.class) {
